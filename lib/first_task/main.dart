2ce17a7a18ced060a43ec08799cae2fd5c76496d
gurwinder-dev <motivationalgk40@gmail.com>
2025-08-01 18:23:20 +0530

    Implemented BottomNavigation

--- lib/first_task/main.dart
+++ lib/first_task/main.dart
@@ -1,21 +1,72 @@
 import 'package:flutter/material.dart';
-
-import 'welcome_screen.dart';
+import '../fifth_task/debate_detailspage.dart';
+import '../fourth_task/debate_feed.dart';
+import '../seventh_task/debate_screen.dart';
 
 void main() {
-  runApp(const HeaticApp());
+  runApp(MyApp());
 }
 
-class HeaticApp extends StatelessWidget {
-  const HeaticApp({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Heatic',
-      home: const WelcomeScreen(),
+      title: 'Debate App',
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: MainScreen(),
     );
   }
 }
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+int myIndex = 0;
+List<Widget> widgetlist=[
+  HeaticDebateFeed(),
+  DebateScreen(),
+  DebateDetailsPage(),
+];
 
+class _MainScreenState extends State<MainScreen> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: myIndex,
+        children: widgetlist,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: myIndex,
+        onTap: (index){
+          setState(() {
+            myIndex=index;
+          });
+        },
+
+        unselectedItemColor: Colors.grey,
+        showUnselectedLabels: true,
+
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_circle_outline),
+            label: 'Create Debate',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+}
